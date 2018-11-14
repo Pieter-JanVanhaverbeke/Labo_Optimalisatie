b7f5d2ab3406185b43470c665ef9ed3857a57ac8
sandero16 <sander.thierens@hotmail.com>
2018-11-14 19:38:06 +0100

    machinematrix

---
+++ src/Machinematrix.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class Machinematrix {
+    private int aantalMachinesid;
+    private int aantalLocatiesid;
+    private int[][] matrix;
+    public Machinematrix(){
+    }
+    public void setAantalMachinesid(int aantalMachinesid){
+        this.aantalMachinesid=aantalMachinesid;
+    }
+    public void setAantalLocatiesid(int aantalLocatiesid){
+        this.aantalLocatiesid=aantalLocatiesid;
+    }
+    public void genereerMatrix(){
+        matrix=new int[aantalLocatiesid][aantalMachinesid];
+        for(int i=0;i<aantalLocatiesid;i++){
+            for(int j=0;j<aantalMachinesid;j++){
+                matrix[i][j]=0;
+            }
+        }
+    }
+    public void setDrop(ArrayList<Drop> drops){
+        for (Drop d: drops) {
+            matrix[d.getLocation().getId()][d.getMachineTypeId()]--;
+        }
+
+    }
+    public void setCollect(ArrayList<Collect> collects){
+        for (Collect c: collects) {
+            matrix[c.getMachine().getLocation().getId()][c.getMachine().getMachineTypeId()]++;
+        }
+
+    }
+    public void dropGebeurt(int locatieId, int machineTypeId){
+        matrix[locatieId][machineTypeId]++;
+    }
+    public void collectGebeurt(int locatieId, int machineTypeId){
+        matrix[locatieId][machineTypeId]--;
+    }
+}
